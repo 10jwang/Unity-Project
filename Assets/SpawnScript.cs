4a0a3d4b0892cbaebf721949e3dcb262ac7abe91
arjunkolani <10akolani@brightoncollege.net>
2024-02-02 11:43:27 +0000

    Adding AI and other scripts
    ...

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject character;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnCharacter()
+    {
+        Instantiate(character, spawnPoint.position, Quaternion.identity);
+        Debug.Log("Hi");
+    }
+}
